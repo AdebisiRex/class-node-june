a3bfd300334e17bd2a7883a4b769e564acb38aa7
Adesina Adebisi <104420789+AdebisiRex@users.noreply.github.com>
2024-07-01 09:48:09 +0100

    SIMPLE CRUD process using EJS

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 2349;
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
-const studentArray = [];
+let studentArray = [];
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -15,7 +15,7 @@ app.post("/submit", (req, res) => {
     res.send("Student too young");
   }
   studentArray.push(student);
-  console.log(studentArray)
+  // console.log(studentArray);
   res.redirect("/students");
 });
 
@@ -23,6 +23,33 @@ app.get("/students", (req, res) => {
   res.render("list", { studentList: studentArray });
 });
 
+app.post("/student/:id", (req, res) => {
+  const { id } = req.params;
+
+  studentArray = studentArray.filter((item, index) => index != id);
+  res.redirect("/students");
+
+  // const array = studentArray.filter((item, index) => index != id);
+  // res.render("list", { studentList: array });
+});
+
+app.get("/editPage/:index", (req, res) => {
+  const { index } = req.params;
+  const selectedStudent = studentArray[index];
+  res.render("editPage", { student: selectedStudent, index: index });
+});
+
+app.post("/updateUser/:index", (req, res) => {
+  const { index } = req.params;
+  const data = req.body;
+  studentArray[index].firstName = data.firstName;
+  studentArray[index].lastName = data.lastName;
+  studentArray[index].age = data.age;
+  studentArray[index].gender = data.gender;
+  studentArray[index].email = data.email;
+
+  res.redirect("/students");
+});
 app.listen(PORT, function () {
   console.log("App started at PORT:" + PORT);
 });
