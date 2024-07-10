86b5bcb5b9bbb0afc5e6859a8c2e1c4e5e23253f
Adesina Adebisi <104420789+AdebisiRex@users.noreply.github.com>
2024-07-10 10:03:52 +0100

    added mongoose methods

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = 2349;
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
-let studentArray = [];
+const MONGO_URI =
+  "mongodb+srv://sqi-abeokuta:SQICollegeOfICT@atlascluster.vbdzuw6.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Mongo DB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const studentSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  gender: String,
+  age: Number,
+});
+
+const studentModel = mongoose.model("students", studentSchema);
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.post("/submit", (req, res) => {
   const student = req.body;
+  console.log(student);
   if (student.age < 18) {
     res.send("Student too young");
   }
-  studentArray.push(student);
-  // console.log(studentArray);
-  res.redirect("/students");
+  const data = new studentModel(student);
+  data
+    .save()
+    .then((saved) => {
+      console.log("Data saved successfully", saved);
+      res.redirect("/students");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-app.get("/students", (req, res) => {
-  res.render("list", { studentList: studentArray });
+app.get("/students", async (req, res) => {
+  // to get data from the database
+
+  const studentData = await studentModel.find();
+
+  // console.log(studentData);
+  res.render("list", { studentList: studentData });
 });
 
 app.post("/student/:id", (req, res) => {
@@ -33,9 +69,11 @@ app.post("/student/:id", (req, res) => {
   // res.render("list", { studentList: array });
 });
 
-app.get("/editPage/:index", (req, res) => {
+app.get("/editPage/:index", async (req, res) => {
   const { index } = req.params;
-  const selectedStudent = studentArray[index];
+
+  const selectedStudent = await studentModel.findOne({ _id: index });
+  // const selectedStudent = studentArray[index];
   res.render("editPage", { student: selectedStudent, index: index });
 });
 
---
+++ lib.js
@@ -0,0 +1,26 @@
+// SQL Databases enforce data structure, NO-SQL Databases allow storage of all kinds of data
+//SQQL enforces data type
+const studentd = [
+  {
+    name: "Tolase",
+    dept: "Web",
+    favCourse: 1,
+    level: 300,
+    heif,
+  },
+  {
+    name: "Tolase",
+    dept: "Web",
+    favCourse: "HTM:",
+  },
+  {
+    name: "Tolase",
+    dept: "Web",
+    favouriteCourse: "HTM:",
+  },
+  {
+    name: "Tolase",
+    dept: "Web",
+    favouriteCourse: "HTM:",
+  },
+];
